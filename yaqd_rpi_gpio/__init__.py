6474a8f6a5ddb66bd15dd1221c7e8c47e1a4c2b8
Blaise Thompson <blaise@untzag.com>
2020-08-15 01:57:24 +0000

    transition to modern yaq

--- yaqd_rpi_gpio/__init__.py
+++ yaqd_rpi_gpio/__init__.py
@@ -1,2 +1,5 @@
+"""yaq daemons for control of Rasperry Pi GPIO"""
+
+
 from .__version__ import *
 from ._gpio_digital_output import *
--- yaqd_rpi_gpio/_gpio_digital_sensor.py
+++ yaqd_rpi_gpio/_gpio_digital_sensor.py
@@ -12,23 +12,20 @@ from .__version__ import __branch__
 
 class GpioDigitalSensor(Sensor):
     _kind = "gpio-digital-sensor"
-    _version = "0.1.0" + f"+{__branch__}" if __branch__ else ""
-    traits: List[str] = []
-    defaults: Dict[str, Any] = {}
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        self.channel_names = config["channels"].keys()
-        self.channel_units = {k: None for k in self.channel_names}
-        self.controllers = []
-        for name in self.channel_names:
+        self._channel_names = config["channels"].keys()
+        self._channel_units = {k: None for k in self._channel_names}
+        self._controllers = []
+        for name in self._channel_names:
             pin = config["channels"][name]["pin"]
             pull_up = config["channels"][name].get("pull_up", False)
             dev = gpiozero.InputDevice(pin=pin, pull_up=pull_up)
-            self.controllers.append(dev)
+            self._controllers.append(dev)
 
     async def _measure(self):
         out = dict()
-        for name, controller in zip(self.channel_names, self.controllers):
+        for name, controller in zip(self._channel_names, self._controllers):
             out[name] = controller.value
         return out
